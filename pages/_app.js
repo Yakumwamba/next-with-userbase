bb642e8e9dfbe8d8f5db1fa38110fe903c8aa493
Yakumwamba <brianlemba2@gmail.com>
2021-10-31 16:21:23 +0200

    Welcome title changed to Ya!

--- pages/_app.js
+++ pages/_app.js
@@ -8,9 +8,9 @@ function MyApp({ Component, pageProps }) {
   const [user, setUser] = useState()
 
   useEffect(() => {
-    userbase.init({ appId: "ffc3d1fc-458d-40a6-9dff-06bab5234992" })
+    userbase.init({ appId: "ffc3d1fc-458d-40a6-9dff-06bab5234992"})
   }, [])
-
+// QKWfk5/IqUwrQjVjPxWV9ZCr+zWVf3te4AyYK1Kgx8I=
   return (
     <Layout user={user} setUser={setUser}>
       <Component user={user} {...pageProps} />
